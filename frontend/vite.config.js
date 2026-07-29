7c64fcd181c44c665c4eb328b830ffa5b9ea4420
slevinnnn <danielbm3012@gmail.com>
2026-07-29 15:54:09 -0400

    se corrigio un problema en el frontend y se agrego el archivo index que faltaba

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,15 +5,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: true,
+    host: '0.0.0.0', // Asegura bind a todas las interfaces
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://backend:8000', // <--- Se usó el nombre del servicio en Docker
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '/socket.io': {
-        target: 'http://localhost:8000',
+        target: 'http://backend:8000', // <--- Se usó el nombre del servicio en Docker
         ws: true,
       }
     }
